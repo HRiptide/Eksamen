7a75582e0c2200f231ff4a24531d09bfe6feffd2
HRiptide <47144013+HRiptide@users.noreply.github.com>
2021-05-17 05:17:56 -0400

    Add files via upload

---
+++ ITS-SE-project-master/web-app/js/util.js
@@ -0,0 +1,7 @@
+var API_KEY = 'Flight%20booker%201.2%20plus%20edition';
+var URL = 'http://127.0.0.1:41062/www/api/API.php?API_KEY='+API_KEY;
+
+var URL_STATUS = 'http://127.0.0.1/www/cgi-bin/status_check?';
+
+//http://46.101.165.227/API.php=?API_KEY='
+
